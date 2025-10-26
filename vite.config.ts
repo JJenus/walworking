7ca128673b65c64c6a8621d91481081b64a5157b
jjenus <biz.jenus@gmail.com>
2025-10-26 09:09:42 +0100

    sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Sitemap from "vite-plugin-sitemap";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+	plugins: [
+		react(),
+		Sitemap({
+			hostname: "https://walworkingtechnologiesltd.com/", // Replace with your domain
+			dynamicRoutes: [
+				"/",
+				"/about",
+				"/services",
+				"/products",
+				"/projects",
+				"/training",
+				"/contact",
+			],
+		}),
+	],
+	optimizeDeps: {
+		exclude: ["lucide-react"],
+	},
 });
